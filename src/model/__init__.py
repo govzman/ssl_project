2c95a9bc5f6fa969ee5bb9910976a72c0bce4c37
Kirill Vekshin <vekshinkir@gmail.com>
2025-12-14 23:04:35 +0300

    BYOL init commit

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from src.model.baseline_model import BaselineModel
 from src.model.BYOL import SSL_BYOL
 from src.model.resnet import ResNetModel
+from src.model.ssl_model import SSLModel
 
-__all__ = ["BaselineModel", "ResNetModel", "SSL_BYOL"]
+__all__ = ["BaselineModel", "ResNetModel", "SSL_BYOL", "SSLModel"]
