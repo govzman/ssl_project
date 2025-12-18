917c4c42ef8acdba8b79977cf78d7dcf22f2b7fa
Ilya Govzman <govzman@list.ru>
2025-12-18 13:18:14 +0300

    Add CIFAR10 dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,2 +1,3 @@
+from src.datasets.cifar10 import CIFAR10
 from src.datasets.example import ExampleDataset
 from src.datasets.stl10 import STL10
---
+++ src/datasets/cifar10.py
@@ -0,0 +1,46 @@
+import json
+import logging
+import random
+from typing import List
+
+import torchvision
+from torchvision.transforms import Resize
+from tqdm import tqdm
+
+from src.datasets.base_dataset import BaseDataset
+from src.utils.io_utils import ROOT_PATH
+
+logger = logging.getLogger(__name__)
+
+
+class CIFAR10(BaseDataset):
+    """
+    CIFAR10
+    """
+
+    def __init__(self, split, *args, **kwargs):
+        """
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+            limit (int | None): if not None, limit the total number of elements
+                in the dataset to 'limit' elements.
+            shuffle_index (bool): if True, shuffle the index. Uses python
+                random package with seed 42.
+            instance_transforms (dict[Callable] | None): transforms that
+                should be applied on the instance. Depend on the
+                tensor name.
+        """
+        data = self._get_or_load_data(split)
+        super().__init__(data, *args, **kwargs)
+
+    def _get_or_load_data(self, split):
+        data_path = ROOT_PATH / "data" / "datasets" / "cifar10"
+        data = torchvision.datasets.CIFAR10(
+            data_path,
+            train=(split == "train"),
+            target_transform=Resize(96),
+            download=True,
+        )
+        return data
